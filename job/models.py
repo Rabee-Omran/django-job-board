fed36098c7e5919ebb3194364abe6727ae5a9dbe
RabeeOmran <rabeeomran2@gmail.com>
2020-08-10 00:26:09 +0300

    update files in job

--- job/models.py
+++ job/models.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
-
+from django.template.defaultfilters import slugify
 
 '''
 django model field
@@ -9,11 +10,19 @@ django model field
  - validation
  - db size
 '''
-JOB_TYPE =(
-    ('Full Time','Full Time'),
-    ('Part Time','Part Time'),
+JOB_TYPE = (
+    ('Full Time', 'Full Time'),
+    ('Part Time', 'Part Time'),
 )
+
+
+def image_upload(instance,filename):
+    imageName, extension = filename.split(".")
+    return "jobs/%s.%s" %(instance.id, extension)
+
+
 class Job(models.Model):
+    owner = models.ForeignKey(User, related_name='job_owner', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     #location
     job_type = models.CharField(max_length=15, choices=JOB_TYPE) 
@@ -24,14 +33,40 @@ class Job(models.Model):
     experience = models.IntegerField(default=1)
     category = models.ForeignKey('Category', on_delete = models.CASCADE)
 
+    image = models.ImageField(upload_to=image_upload)
+
+    slug = models.SlugField(blank = True,null=True )
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Job, self).save(*args, **kwargs)
+
+
+
     def __str__(self):
         return self.title
+
 
 class Category(models.Model):
     name =  models.CharField(max_length=25) 
 
     def __str__(self):
         return self.name
+
+
+class Apply(models.Model):
+    job = models.ForeignKey(Job,related_name='apply', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    website = models.URLField()
+    cv = models.FileField(upload_to='apply/')
+    cover_letter = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
 
     
     
